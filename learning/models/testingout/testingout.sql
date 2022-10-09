31cb4a092a24d72453d4a4c1bac152db19004f92
everyonehatesLin3lis <peleckas.l@gmail.com>
2022-10-09 19:03:24 +0300

    changed the SQL querry

--- learning/models/testingout/testingout.sql
+++ learning/models/testingout/testingout.sql
@@ -1,3 +1,5 @@
-SELECT json_data -> 'glossary' ->> 'title' AS title, 
-    json_data -> 'glossary' -> 'GlossDiv' -> 'GlossList' -> 'GlossEntry' ->> 'ID' AS id
-FROM dbt.testing
+SELECT _airbyte_data ->> 'id' AS id, 
+    _airbyte_data ->> 'currency' AS currency,
+    _airbyte_data ->> 'refunds' AS refunds,
+    _airbyte_data -> 'customer' ->> 'id' as customer_id
+FROM test._airbyte_raw_orders
